7510e1fb83cf79a614530e840c7ab3323bb05296
JanPavlin <jp8765@student.uni-lj.si>
2019-12-16 09:41:24 +0100

    renamed fileDownloader

--- api/src/main/java/si/fri/rso/api/v1/fileDownloader.java
+++ api/src/main/java/si/fri/rso/api/v1/fileDownloader.java
@@ -4,11 +4,9 @@ import com.kumuluz.ee.discovery.annotations.RegisterService;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @RegisterService(value = "rso1920-fileDownloader")
 @ApplicationPath("/v1")
-public class FileDownloader extends Application{
+public class fileDownloader extends Application{
 
 }
